921c2bc0a884f9d31cae236e4b15306581fa1137
yashraikar1087 <yashraikar1087@gmail.com>
2019-03-24 17:12:58 -0600

    axis

---
+++ barchart/index1.js
@@ -0,0 +1,33 @@
+var data = [80, 100, 56, 120, 180, 30, 50, 120, 160];
+
+var svgWidth = 500,
+  svgHeight = 300,
+  barPadding = 5;
+
+var svg = d3
+  .select("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var yScale = d3
+  .scaleLinear()
+  .domain([0, d3.max(data)])
+  .range([0, svgHeight]);
+
+var xScale = d3
+  .scaleLinear()
+  .domain([0, d3.max(data)])
+  .range([0, svgWidth]);
+
+var x_axis = d3.axisBottom().scale(xScale);
+var y_axis = d3.axisLeft().scale(yScale);
+svg
+  .append("g")
+  .attr("transform", "translate(50,10)")
+  .call(y_axis);
+var xAxisTranslate = svgHeight - 20;
+
+svg
+  .append("g")
+  .attr("transform", "translate(50, " + xAxisTranslate + ")")
+  .call(x_axis);

c5e64e87aa2e1ff822aa90782260782b06fce88b
yashraikar1087 <yashraikar1087@gmail.com>
2019-03-24 16:58:53 -0600

    scaling

--- barchart/index.js
+++ barchart/index.js
@@ -11,19 +11,40 @@ var svg = d3
   .attr("width", svgWidth)
   .attr("height", svgHeight);
 
+var yScale = d3
+  .scaleLinear()
+  .domain([0, d3.max(dataset)])
+  .range([0, svgHeight]);
+
 var barChart = svg
   .selectAll("rect")
   .data(dataset)
   .enter()
   .append("rect")
   .attr("y", function(d) {
-    return svgHeight - d;
+    return svgHeight - yScale(d);
   })
   .attr("height", function(d) {
-    return d;
+    return yScale(d);
   })
   .attr("width", barWidth - barPadding)
   .attr("transform", function(d, i) {
     var translate = [barWidth * i, 0];
     return "translate(" + translate + ")";
   });
+
+var text = svg
+  .selectAll("text")
+  .data(dataset)
+  .enter()
+  .append("text")
+  .text(function(d) {
+    return d;
+  })
+  .attr("y", function(d, i) {
+    return svgHeight - d - 2;
+  })
+  .attr("x", function(d, i) {
+    return barWidth * i;
+  })
+  .attr("fill", "red");
